dda3002b4e366e01a9e54ec527c887d9ed7b8c38
therealjoyesh <joyesh708@gmail.com>
2017-05-03 13:23:52 -0500

    Inital commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# REST Chat Protocol
+REST Chat Protocol is a chatting API/server/thing that's simple and easy to use.
+
+It's written in Python 3 using Flask. That's all.
+
+I don't really know how to write descriptons yay
---
+++ RECS_Server.py
@@ -0,0 +1,41 @@
+#!flask/bin/python
+
+# RECS | REST Chat Server
+# Server runner
+# Built off of Flask and Python by Joyesh.
+
+# Imports
+from flask import Flask, jsonify, request
+
+# RECS Flask app
+app = Flask(__name__)
+
+messages = [
+    {
+        'id': 1,
+        'authorid': 1,
+        'content': u'Hello world!'
+    },
+]
+
+# Get all messages
+@app.route('/recs/api/v1.0/message', methods=['GET'])
+def get_messages():
+    return jsonify({'messages': messages})
+
+# Add message
+@app.route('/recs/api/v1.0/message', methods=['POST'])
+def send_message():
+    if not request.json or not 'authorid' in request.json or not 'content' in request.json:
+        abort(400)
+
+    message = {
+        'id': messages[-1]['id'] + 1,
+        'authorid': request.json['authorid'],
+        'content': request.json['content']
+    }
+    messages.append(message)
+    return jsonify({'message': message}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
