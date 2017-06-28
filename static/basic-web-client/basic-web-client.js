9c277bfd8704861bb215d8c96e07eda099b76d5a
= <joyesh708@gmail.com>
2017-06-28 13:39:38 -0500

    add onerror

--- static/basic-web-client/basic-web-client.js
+++ static/basic-web-client/basic-web-client.js
@@ -64,3 +64,8 @@ function editmsg(id) {
     update();
   }
 }
+
+window.onerror = function(msg, url, linenumber) {
+    alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
+    return true;
+}
